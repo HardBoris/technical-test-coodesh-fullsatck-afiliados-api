acc8a2ccc66869de44fce87010923001676aae0b
HardBoris <bgaibor03@gmail.com>
2023-07-21 10:47:59 -0400

    refactor: data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,14 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import path from "path";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  logging: false,
+  ssl: { rejectUnauthorized: false },
+  entities: [path.join(__dirname, `./entities/**/*.{js,ts}`)],
+  migrations: [path.join(__dirname, `./migrations/**/*.{js,ts}`)],
+});
